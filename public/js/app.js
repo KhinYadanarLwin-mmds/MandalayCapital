1a4690d40620ae45448c4043332500b5d427cae5
Khin_Yadanar_Lwin <kyadanarlwin2511@gmail.com>
2020-08-30 14:44:10 +0630

    Sun

--- public/js/app.js
+++ public/js/app.js
@@ -19384,8 +19384,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\DELL\Desktop\MandalayCapital\MdyCapProject\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\DELL\Desktop\MandalayCapital\MdyCapProject\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\Hsu Myat\Desktop\Projects\MandalayCapital\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\Hsu Myat\Desktop\Projects\MandalayCapital\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
